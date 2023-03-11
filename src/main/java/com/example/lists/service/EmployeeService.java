f3ef358fcca748942476dd94ebd83b54181dfb6f
Sazonov Daniil <ter6062@ya.ru>
2023-03-11 11:24:05 +0300

    2.8

--- src/main/java/com/example/lists/service/EmployeeService.java
+++ src/main/java/com/example/lists/service/EmployeeService.java
@@ -7,36 +7,34 @@ import com.example.lists.object.Employee;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
     private final int MAX_COUNT_OF_EMPLOYEE = 10;
-    Map<String, Employee> employees = new HashMap<>();
+    private final Map<String, Employee> employees = new HashMap<>();
 
     //МЕТОД УДАЛЕНИЯ СОТРУДНИКА ИЗ МАССИВА
     public Employee delEmployee(String firstName, String lastName) {
-        Employee employee = new Employee(firstName, lastName);
-        if (employees.containsKey(employee.getFirstName() + " " + employee.getLastName())) {
-            employees.remove(employee);
-            return employee;
-        } else {
+        if (!employees.containsKey(firstName + " " + lastName)) {
             throw new EmployeeNotFoundException("Сотрудник не найден");
         }
+
+        return employees.remove(firstName + " " + lastName);
+
     }
 
     //МЕТОД ДОБАВЛЕНИЯ СОТРУДНИКА ИЗ МАССИВА
-    public Employee addEmployee(String firstName, String lastName) {
+    public Employee addEmployee(String firstName, String lastName, int sallary, int departament) {
         if (employees.size() >= MAX_COUNT_OF_EMPLOYEE) {
             throw new EmployeeStorageIsFullException("Массив переполнен");
         }
-        Employee employee = new Employee(firstName, lastName);
+        Employee employee = new Employee(firstName, lastName, sallary, departament);
         if (employees.containsKey(employee.getFirstName() + " " + employee.getLastName())) {
             throw new EmployeeAlreadyAddedException("Сотрудник уже добавлен");
         }
         employees.put(employee.getFirstName() + " " + employee.getLastName(), employee);
         return employee;
-
-
     }
 
     //МЕТОД ПОИСКА СОТРУДНИКА В МАССИВА
@@ -49,7 +47,34 @@ public class EmployeeService {
         }
     }
 
+    public Employee findMaxSallary(int department) {
+        return employees.values().stream()
+                .filter(employee -> employee.getDepartament() == department)
+                .max(Comparator.comparing(Employee::getSallary))
+                .orElseThrow(() -> new EmployeeNotFoundException("Сотрудник не найден"));
+    }
+
+    public Employee findMinSalary(int department) {
+        return employees.values().stream()
+                .filter(employee -> employee.getDepartament() == department)
+                .min(Comparator.comparing(Employee::getSallary))
+                .orElseThrow(() -> new EmployeeNotFoundException("Сотрудник не найден"));
+    }
+
+    public Collection<Employee> allOfOneDepartment(int department) {
+        return employees.values().stream()
+                .filter(employee -> employee.getDepartament() == department)
+                .collect(Collectors.toList());
+    }
+
+
     public Collection<Employee> showAll() {
         return Collections.unmodifiableCollection(employees.values());
+    }
+
+    public Collection<Employee> showAllDepartment() {
+        return employees.values().stream()
+                .sorted(Comparator.comparing(Employee::getDepartament))
+                .collect(Collectors.toList());
     }
 }
